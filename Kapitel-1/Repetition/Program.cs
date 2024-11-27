36f04d3ef19303dad90a2abd29b8fd2a92251bc9
Ingrid Gistorp <ingrid.gistorp@elev.ga.ntig.se>
2024-11-27 15:02:19 +0100

    Tränat på att ställa frågor i form av variabler och tryparse

---
+++ Kapitel-1/Repetition/Program.cs
@@ -0,0 +1,14 @@
+﻿// Program
+Console.Clear();
+Console.WriteLine("HejHej");
+Console.WriteLine("Vad heter du");
+String name = Console.ReadLine();
+Console.WriteLine($"Hej,{name}");
+
+Console.WriteLine("Hur gammal är du");
+int.TryParse(Console.ReadLine(), out int age);
+Console.WriteLine($"Oj du är {age}");
+
+Console.WriteLine("Vad är din favorit färg");
+String color = Console.ReadLine();
+Console.WriteLine($"Aha, du gillar {color}");
